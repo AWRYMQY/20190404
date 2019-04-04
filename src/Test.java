bc0290b698dd6e87c9b76278dd33b64db15ee482
AWRYMQY <awrymqy97@gmail.com>
2019-04-04 17:03:01 +0800

    20190404

--- src/Test.java
+++ src/Test.java
@@ -8,7 +8,7 @@ public class Test {
 		//BaseClass basic = new BaseClass(username, password, null, null);
 		BaiduPanService service = new BaiduPanService(username, password, null);
 		//service.quota(null);
-		service.listFiles("/2", null, null, null, null, null);
+		//service.listFiles("/2", null, null, null, null, null);
 		//service.getThumbnail("/2/fe5594209ab28b2c68e4f021437e2d23.jpg", null, null, null, null);
 		
 	}
